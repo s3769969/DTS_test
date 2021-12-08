0dd50f7e6a7e853722c5d850c8615bc74037d1d8
Sebastian <49255607+s3769969@users.noreply.github.com>
2021-12-08 14:59:02 +1100

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,131 +1,270 @@
+var presets = {
+"PRESETS": [{
+"VIEW_NAME": "ICU",
+"UR": true,
+"ENCNTR_ID": false,
+"MRN": true,
+"NAME_FULL_FORMATTED": true,
+"BIRTH_DT_TM": true,
+"SEX": true,
+"RESULT": true,
+"RESULT_FLAG": false,
+"HAZARDOUS": true,
+"HOSPITAL_SITE": true,
+"WARD/AREA": true,
+"BED_NO": true,
+"ADMITTING_SPECIALTY": false,
+"ADMITTING_CONSULTANT": false,
+"PATIENT_DETAILS": true
+}, {
+"VIEW_NAME": "ED",
+"UR": true,
+"ENCNTR_ID": false,
+"MRN": true,
+"NAME_FULL_FORMATTED": true,
+"BIRTH_DT_TM": true,
+"SEX": true,
+"RESULT": true,
+"RESULT_FLAG": false,
+"HAZARDOUS": true,
+"HOSPITAL_SITE": true,
+"WARD/AREA": true,
+"BED_NO": true,
+"ADMITTING_SPECIALTY": true,
+"ADMITTING_CONSULTANT": true,
+"PATIENT_DETAILS": true
+}, {
+"VIEW_NAME": "ORTHOPAEDICS",
+"UR": true,
+"ENCNTR_ID": false,
+"MRN": true,
+"NAME_FULL_FORMATTED": true,
+"BIRTH_DT_TM": true,
+"SEX": true,
+"RESULT": false,
+"RESULT_FLAG": false,
+"HAZARDOUS": false,
+"HOSPITAL_SITE": true,
+"WARD/AREA": true,
+"BED_NO": true,
+"ADMITTING_SPECIALTY": true,
+"ADMITTING_CONSULTANT": true,
+"PATIENT_DETAILS": true
+}]
+}
+
 var list = {
 "REC": {
 "PAT": [{
-"PERSON_ID": 733924.000000,
+"UR": 733924.000000,
 "ENCNTR_ID": 0.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Git, Hub ",
 "BIRTH_DT_TM": "\/Date(1990-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "F",
 "RESULT": "1",
 "RESULT_FLAG": "res-low",
 "HAZARDOUS": "true",
 "HOSPITAL_SITE": "Clayton",
-"WARD/AREA": "ICU"
+"WARD/AREA": "ICU",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 2607950.000000,
+"UR": 2607950.000000,
 "ENCNTR_ID": 0.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "John, Luke ",
 "BIRTH_DT_TM": "\/Date(1991-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "M",
 "RESULT": "10",
 "RESULT_FLAG": "res-normal",
 "HAZARDOUS": "false",
 "HOSPITAL_SITE": "Dandenong",
-"WARD/AREA": "ED"
+"WARD/AREA": "ED",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 2601764.000000,
+"UR": 2601764.000000,
 "ENCNTR_ID": 0.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Pierce, Pierce ",
 "BIRTH_DT_TM": "\/Date(1992-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "M",
 "RESULT": "10",
 "RESULT_FLAG": "res-normal",
 "HAZARDOUS": "false",
 "HOSPITAL_SITE": "Clayton",
-"WARD/AREA": "ED"
+"WARD/AREA": "ED",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 2605750.000000,
+"UR": 2605750.000000,
 "ENCNTR_ID": 0.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Hello, World ",
 "BIRTH_DT_TM": "\/Date(1993-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "F",
 "RESULT": "10",
 "RESULT_FLAG": "res-normal",
 "HAZARDOUS": "false",
 "HOSPITAL_SITE": "Moorabbin",
-"WARD/AREA": "ED"
+"WARD/AREA": "ED",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 2605755.000000,
+"UR": 2605755.000000,
 "ENCNTR_ID": 0.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Saint, Vincent ",
 "BIRTH_DT_TM": "\/Date(1994-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "M",
 "RESULT": "10",
 "RESULT_FLAG": "res-normal",
 "HAZARDOUS": "false",
 "HOSPITAL_SITE": "Clayton",
-"WARD/AREA": "ED"
+"WARD/AREA": "ED",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 2677951.000000,
+"UR": 2677951.000000,
 "ENCNTR_ID": 0.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Jeffrey, Jeff ",
 "BIRTH_DT_TM": "\/Date(1995-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "M",
 "RESULT": "10",
 "RESULT_FLAG": "res-normal",
 "HAZARDOUS": "false",
 "HOSPITAL_SITE": "Clayton",
-"WARD/AREA": "ED"
+"WARD/AREA": "ED",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 2631972.000000,
+"UR": 2631972.000000,
 "ENCNTR_ID": 1.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Han, Solo ",
 "BIRTH_DT_TM": "\/Date(1996-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "M",
 "RESULT": "1000",
 "RESULT_FLAG": "res-severe",
 "HAZARDOUS": "true",
 "HOSPITAL_SITE": "Clayton",
-"WARD/AREA": "ICU"
+"WARD/AREA": "ICU",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 2637956.000000,
+"UR": 2637956.000000,
 "ENCNTR_ID": 2.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Armstrong, Niel ",
 "BIRTH_DT_TM": "\/Date(1997-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "M",
 "RESULT": "100",
 "RESULT_FLAG": "res-high",
 "HAZARDOUS": "false",
 "HOSPITAL_SITE": "Clayton",
-"WARD/AREA": "ICU"
+"WARD/AREA": "ICU",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 873923.000000,
+"UR": 873923.000000,
 "ENCNTR_ID": 0.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Ice, Tea ",
 "BIRTH_DT_TM": "\/Date(1998-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "M",
 "RESULT": "100",
 "RESULT_FLAG": "res-high",
 "HAZARDOUS": "false",
 "HOSPITAL_SITE": "Clayton",
-"WARD/AREA": "ID"
+"WARD/AREA": "ID",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"
 }, {
-"PERSON_ID": 893723.000000,
+"UR": 893723.000000,
 "ENCNTR_ID": 0.000000,
 "MRN": "Active",
 "NAME_FULL_FORMATTED": "Chapelle, Dave ",
 "BIRTH_DT_TM": "\/Date(1999-01-01T00:00:00.000+00:00)\/",
-"SEX_CD": 0.000000,
+"SEX": "M",
 "RESULT": "1",
 "RESULT_FLAG": "res-low",
 "HAZARDOUS": "false",
 "HOSPITAL_SITE": "Clayton",
-"WARD/AREA": "AMU"
+"WARD/AREA": "AMU",
+"BED_NO": "6",
+"ADMITTING_SPECIALTY": "General Medicine",
+"ADMITTING_CONSULTANT": "Doogie Howser",
+"PATIENT_DETAILS": "Click to expand"	
 }]
 }};
 
 //import { hello } from './module.js';
 //import list from './script.js';
 //import list from 'http://127.0.0.1:5500/script.js';
+
+//manage column visibility
+var colVisibility = Object.keys(list.REC.PAT[0]);
+if(localStorage.colVisibility === undefined) {
+	for (i = 0; i < colVisibility.length; i++){
+		if (colVisibility[i].endsWith("FLAG")){
+			colVisibility[i] = false;
+		}else{
+			colVisibility[i] = true;
+		}
+	}
+	localStorage.setItem("colVisibility", JSON.stringify(colVisibility));
+}else{
+	colVisibility = JSON.parse(localStorage.getItem("colVisibility"));
+}
+console.log("initial:", JSON.parse(localStorage.getItem("colVisibility")));
+//console.log("colVisibility:" + colVisibility);
+
+
+
+
+//Set the width of the sidebar to 250px and the left margin of the page content to 250px
+function openNav1() {
+  document.getElementById("mySidebar").style.paddingLeft = "5px";
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "270px";
+}
+
+function closeNav1() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+//Set the width of the sidebar to 250px and the left margin of the page content to 250px
+function openNav2() {
+  document.getElementById("mySidebar2").style.paddingLeft = "5px";
+  document.getElementById("mySidebar2").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "270px";
+}
+
+function closeNav2() {
+  document.getElementById("mySidebar2").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
 
 //create each option for drop down list using list object, text to display and option value
 function createOption(ddl, text, value) {
@@ -248,6 +387,7 @@ function filterLoc(selector) {
 
 //construct table object using json object for data
 function constructTable(selector) {
+	
 	//console.log("creating table");
 	//console.log(list.REC.PAT[0]);
 	document.getElementById("table").innerHTML = "";
@@ -259,7 +399,7 @@ function constructTable(selector) {
 	
 	//fill table cells
 	let dob = /BIRTH_DT_TM/;
-	let year, month, day, dateString;
+	let year, month, day, dateString, displayFormat;
 	for (let i = 0; i < list.REC.PAT.length; i++) {
 		const tr = tbl.insertRow();
 		for (let j = 0; j < headings.length; j++) {
@@ -271,7 +411,8 @@ function constructTable(selector) {
 				day = list.REC.PAT[i][headings[j]].substring(14, 16);
 				dateString = year + month + day;
 				td.sortValue = dateString;
-				td.appendChild(document.createTextNode(list.REC.PAT[i][headings[j]].substring(6, 16)));				
+				displayFormat = day + "-" + month + "-" + year;
+				td.appendChild(document.createTextNode(displayFormat));				
 			}else{
 				td.appendChild(document.createTextNode(list.REC.PAT[i][headings[j]]));
 				td.sortValue = list.REC.PAT[i][headings[j]].toString();
@@ -292,6 +433,20 @@ function constructTable(selector) {
 					td.className = list.REC.PAT[i][headings[j+1]];
 				}
 			}
+			//add link for patient details
+			if(/PATIENT_DETAILS/.test(headings[j])){
+				td.innerHTML = "";
+				var a = document.createElement('a');
+				var linkText = document.createTextNode("Click for patient details");
+				a.appendChild(linkText);
+				a.title = "Patient details link";
+				a.href = "./details.html?" + i;
+				td.appendChild(a);
+				function fu() {
+					var opened = window.open("");
+				    opened.document.write("Your HTML here");
+				}
+			}
 		}
 	}
 	
@@ -304,21 +459,141 @@ function constructTable(selector) {
 		cell.onclick =  function() { sortTable(i); };
 		cell.innerHTML = headings[i];
 		cell.style.fontWeight = 'bold';
+		cell.className = "ascdesc";
 	}
 	
 	//hide flag columns
 	var tableRows = document.getElementById("table").getElementsByTagName("tr");
-	for (i = 0; i < tableRows.length; i++) {
+	for (var i = 0; i < tableRows.length; i++) {
 		for (var j = 0; j < tableRows[i].cells.length; j++) {
-			tableRows[i].cells[j].style.display = "";
-			if (headings[j].endsWith("FLAG"))
+			//console.log("checking", localStorage.getItem("colVisibility"), localStorage.getItem("colVisibility"), tbl.rows[i].cells[j]);
+			if (!JSON.parse(localStorage.getItem("colVisibility"))[j]){
+				//console.log(j, JSON.parse(localStorage.getItem("colVisibility"))[j]);
 				tbl.rows[i].cells[j].style.display = "none";
 			}
+		}
 	}
-	document.body.appendChild(tbl);
+	document.getElementById("main").appendChild(tbl);
 	constructDropDown("cols");
 	constructDropDown("hospitals");
 	constructDropDown("ward/areas");
+}
+
+//load settings sidebar
+function loadSettings(){
+	//console.log("loading settings sidebar");
+	var headings = Object.keys(list.REC.PAT[0]);
+	var tracker = document.getElementById("closeNavbtn");
+	var separator = document.createElement('br');
+	separator.className = "separator";
+	tracker.parentNode.insertBefore(separator,tracker.nextSibling);
+	var checkBox, label;
+	for (i = headings.length-1; i > -1; i--	){
+		checkBox = document.createElement('input');
+		label = document.createElement('label');
+		checkBox.type = "checkbox";
+		checkBox.name = headings[i];
+		checkBox.value = headings[i];
+		checkBox.id = i + ". " + headings[i];
+		if(JSON.parse(localStorage.getItem("colVisibility"))[i]){	
+			checkBox.defaultChecked = true;
+		}
+		label.htmlFor = checkBox.id;
+		label.style = "display:block";
+		label.appendChild(checkBox);
+		label.appendChild(document.createTextNode(headings[i]));
+		tracker.parentNode.insertBefore(label,tracker.nextSibling);
+		//checkBox.parentNode.insertBefore(label,checkBox.nextSibling);
+		if(headings[i].endsWith("FLAG")){
+			checkBox.style.display = "none";
+			label.style.display = "none";
+		}
+	}
+	label = document.createElement('label');
+	label.style = 'font-weight:bold; font-size:larger; margin-bottom: 10px;';
+	label.appendChild(document.createTextNode("Visible Columns"));
+	tracker.parentNode.insertBefore(label,tracker.nextSibling);
+	separator = document.createElement('br');
+	separator.className = "separator";
+	tracker.parentNode.insertBefore(separator,tracker.nextSibling);
+}
+
+//update values based on user settings
+function updateSettings(settings){
+	console.log(event);
+	for (i = 0; i < Object.keys(list.REC.PAT[0]).length; i++){
+		colVisibility[i] = event.target[i].checked;
+	}
+	localStorage.setItem("colVisibility", JSON.stringify(colVisibility));
+	//console.log("colVisibility:",colVisibility);
+	console.log("update check:", JSON.parse(localStorage.getItem("colVisibility")));
+	//updateColumnVisibility();
+	//constructTable("table");
+	//can add validation later
+	return true;	
+}
+
+//update column visibility
+function showHideColumn(colNumber, visible){
+	colVisibility[colNumber] = visible;
+}
+
+function updateColumnVisibility(){
+	var tableRows = document.getElementById("table").getElementsByTagName("tr");
+	for (var i = 0; i < tableRows.length; i++) {
+		for (var j = 0; j < tableRows[i].cells.length; j++) {
+			tableRows[i].cells[j].style.display = "";
+			if (!colVisibility[j]){	
+				tbl.rows[i].cells[j].style.display = "none";
+			}
+		}
+	}
+}
+
+//load presets sidebar
+function loadPresets(){
+	//console.log("loading presets sidebar");
+	var headings = Object.keys(list.REC.PAT[0]);
+	var tracker = document.getElementById("presetsArea");
+	var checkBox, label;
+	for (var i = presets.PRESETS.length-1; i > -1; i--){
+		//create preset selection
+		checkBox = document.createElement('input');
+		label = document.createElement('label');
+		checkBox.type = "radio";
+		checkBox.name = "presets";
+		checkBox.value = presets.PRESETS[i].VIEW_NAME;
+		checkBox.id = i + ". " + presets.PRESETS[i].VIEW_NAME;
+		checkBox.style = "{display:block}";
+		checkBox.onchange = function() { selectPreset(this.id); };
+		label.htmlFor = checkBox.id;
+		label.appendChild(checkBox);
+		label.appendChild(document.createTextNode(presets.PRESETS[i].VIEW_NAME));
+		label.style = "{display:block}";
+		tracker.parentNode.insertBefore(label,tracker.nextSibling);
+	}
+	label = document.createElement('label');
+	label.style = 'font-weight:bold; font-size:larger; margin-bottom: 10px;';
+	label.appendChild(document.createTextNode("Select Preset"));
+	tracker.parentNode.insertBefore(label,tracker.nextSibling);
+}
+
+//update column visibility form using presets
+function selectPreset(preset_id){
+	//console.log("loading presets sidebar");
+	var trimmmedID = preset_id.split(".")[0];
+	var headings = Object.keys(list.REC.PAT[0]);
+	var checkBox, name;
+	//fill form with preset data
+	for (var j = headings.length-1; j > -1; j--	){
+		name = j + ". " + headings[j];
+		checkBox = document.getElementById(name);
+		if(presets.PRESETS[trimmmedID][headings[j]]){	
+			checkBox.checked = true;
+		}else{
+			checkBox.checked = false;
+		}
+	}
 }
 
 //search table using selected field from filterDropDown object
@@ -359,6 +634,11 @@ function sortTable(n) {
 	//console.log("sorting column " + n);
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
 	table = document.getElementById("table");
+	//reset all direction arrows
+	for (var a = 0; a < colVisibility.length; ++a){
+		table.children[0].children[0].children[a].className = "ascdesc";
+	}
+	//console.log(table.children[0].children[0]);
 	switching = true;
 	// Set the sorting direction to ascending:
 	dir = "asc";
@@ -380,12 +660,14 @@ function sortTable(n) {
 		/* Check if the two rows should switch place,
 		based on the direction, asc or desc: */
 		if (dir == "asc") {
+			table.children[0].children[0].children[n].className = "asc";
 			if (x.sortValue.toLowerCase() > y.sortValue.toLowerCase()) {
 				// If so, mark as a switch and break the loop:
 				shouldSwitch = true;
 				break;
 			}
 		}else if (dir == "desc") {
+			table.children[0].children[0].children[n].className = "desc";
 			if (x.sortValue.toLowerCase() < y.sortValue.toLowerCase()) {
 				// If so, mark as a switch and break the loop:
 				shouldSwitch = true;
@@ -411,4 +693,71 @@ function sortTable(n) {
 	  }
 }
 
+
+//construct details table object using json object for data
+function constructDetailsTable(selector) {
+	
+	//console.log("creating table");
+	//console.log(list.REC.PAT[0]);
+	document.getElementById("table").innerHTML = "";
+	//style table element
+    var tbl = document.getElementById("table");
+	tbl.style.width = '150px';
+	tbl.style.border = '1px solid black';
+	const headings = Object.keys(list.REC.PAT[0]);
+	
+	//fill table cells
+	let dob = /BIRTH_DT_TM/;
+	let year, month, day, dateString;
+		var queryString = location.search.substring(1);
+		//alert(queryString);
+		var i = queryString;
+		for (let j = 0; j < headings.length; j++) {
+			const tr = tbl.insertRow();
+			td = tr.insertCell();
+			td.innerHTML = headings[j];
+			td.style.fontWeight = 'bold';
+			//hide patient details heading
+			if(/PATIENT_DETAILS/.test(headings[j])){
+				td.style.display = "none"
+			}
+			td = tr.insertCell();
+			//change sort and display values for DOB field
+			if(dob.test(headings[j])){
+				year = list.REC.PAT[i][headings[j]].substring(6, 10);
+				month = list.REC.PAT[i][headings[j]].substring(11, 13);
+				day = list.REC.PAT[i][headings[j]].substring(14, 16);
+				dateString = year + month + day;
+				td.sortValue = dateString;
+				td.appendChild(document.createTextNode(list.REC.PAT[i][headings[j]].substring(6, 16)));				
+			}else{
+				td.appendChild(document.createTextNode(list.REC.PAT[i][headings[j]]));
+				td.sortValue = list.REC.PAT[i][headings[j]].toString();
+			}
+			//mark className for HAZARDOUS to allow css styling
+			if(/HAZARDOUS/.test(headings[j])){
+				if(/true/.test(list.REC.PAT[i][headings[j]])){					
+					td.className = "hazardTrue";
+					//td.innerHTML += '<img align="center" width="100%" height="100%" src="http://dummyimage.com/68x68/000/fff" />';
+				}else{
+					td.className = "hazardFalse";
+				}
+			}
+			td.style.border = '1px solid black';
+			//if next heading is a FLAG for conditional formatting use value as a className
+			if(j+1 < headings.length){
+				if(headings[j+1].endsWith("FLAG")){
+					td.className = list.REC.PAT[i][headings[j+1]];
+				}
+			}
+			//hide patient details
+			if(/PATIENT_DETAILS/.test(headings[j])){
+				td.style.display = "none"
+			}
+	}
+	document.getElementById("main").appendChild(tbl);
+	constructDropDown("cols");
+	constructDropDown("hospitals");
+	constructDropDown("ward/areas");
+}
 
